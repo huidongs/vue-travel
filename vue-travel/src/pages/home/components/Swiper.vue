b56dacd96146215485e1386cfe9b028d1cca93e9
huidongs <2804234243@qq.com>
2020-09-09 09:51:37 +0800

    commit

--- vue-travel/src/pages/home/components/Swiper.vue
+++ vue-travel/src/pages/home/components/Swiper.vue
@@ -1,11 +1,11 @@
 <template>
-<div class="wrapper">
-  <swiper :options="swiperOptions">
-    <swiper-slide v-for="item of swiperList" :key="item.id">
-      <img class="swiper-img" :src="item.imgUrl" />
-    </swiper-slide>
-    <div class="swiper-pagination" slot="pagination"></div>
-  </swiper>
+  <div class="wrapper">
+    <swiper :options="swiperOptions">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 <script>
@@ -34,14 +34,12 @@ export default {
 <style lang="stylus" scoped>
 .wrapper.swiper-pagination-bullet-active
   background: #FFF
-.wrapper{
-  overflow :hidden
-  width:100%
-  height:0
-  padding-bottom :31.25%
-  background:#eee
-  .swiper-img{
-    width :100%
-  }
-}
+.wrapper
+  overflow: hidden
+  width: 100%
+  height: 0
+  padding-bottom: 31.25%
+  background: #eee
+  .swiper-img
+    width: 100%
 </style>
