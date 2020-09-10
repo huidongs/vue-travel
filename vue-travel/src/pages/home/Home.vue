a5ed164a22a9ba82aa116db7628fe531aea98ed9
huidongs <2804234243@qq.com>
2020-09-10 16:39:43 +0800

    commit

--- vue-travel/src/pages/home/Home.vue
+++ vue-travel/src/pages/home/Home.vue
@@ -2,16 +2,24 @@
    <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <home-recommend></home-recommend>
    </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
