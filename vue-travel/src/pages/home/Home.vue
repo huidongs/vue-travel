1e898f9b5a6b511915620dbc5a41b14173268bc3
huidongs <2804234243@qq.com>
2020-09-28 17:36:35 +0800

    commit

--- vue-travel/src/pages/home/Home.vue
+++ vue-travel/src/pages/home/Home.vue
@@ -13,6 +13,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
   name: 'Home',
   components: {
